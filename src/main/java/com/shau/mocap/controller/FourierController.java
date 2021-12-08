6c9594f11f2644c5d7d9a5aa82d0ce4ffa22f99a
Scott Hare <scotthare@Scotts-MacBook-Pro.local>
2021-12-08 13:50:55 +0000

    removed rest controller anotation

--- src/main/java/com/shau/mocap/controller/FourierController.java
+++ src/main/java/com/shau/mocap/controller/FourierController.java
@@ -9,13 +9,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.charset.StandardCharsets;
 
-@RestController
+@Controller
 public class FourierController {
 
     private FourierService fourierService;
--- src/main/java/com/shau/mocap/controller/UploadController.java
+++ src/main/java/com/shau/mocap/controller/UploadController.java
@@ -6,6 +6,7 @@ import com.shau.mocap.service.ParserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@RestController
+@Controller
 public class UploadController {
 
     private ParserService parserService;
